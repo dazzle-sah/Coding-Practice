c9f7f6521c1d5246a530b076133de6ef16a625e6
dazzle-sah <drishti.sah1412@gmail.com>
2023-08-18 18:41:27 +0530

    prime

--- prime.cpp
+++ prime.cpp
@@ -1,2 +1,27 @@
 //check whether a number is prime or not
 
+#include <iostream>
+using namespace std;
+
+int prime(int k) {
+    if (k <= 1) {
+        cout << "Neither Prime nor Composite";
+        return 0;
+    }
+
+    for (int i = 2; i < k; i++) {
+        if (k % i == 0) {
+            cout << "Composite";
+            return 0;
+        }
+    }
+    
+    cout << "Prime";
+    return 0;
+}
+int main() {
+    int n;
+    cin>>n;
+prime(n);
+    return 0;
+}
