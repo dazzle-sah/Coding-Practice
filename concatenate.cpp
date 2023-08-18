fd4b407061fb620f99d677ce0bf927b05b52f974
dazzle-sah <drishti.sah1412@gmail.com>
2023-08-18 11:51:55 +0530

    concatenate

---
+++ concatenate.cpp
@@ -0,0 +1,14 @@
+// Concatenate 2 words -
+
+#include <iostream>
+using namespace std;
+int concat(string a,string b){
+  cout<<a+b;
+ return 0;
+}
+int main() {
+   string m,n;
+   cin>>m>>n;
+   concat(m,n);
+    return 0;
+}
